7a1c49389b44bef91c72d48c15ef4b8e0131892b
Shiny <106364725+ShinyBread@users.noreply.github.com>
2023-02-28 20:38:49 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,39 +1,36 @@
 import discord
 from itertools import cycle
-from listabaneados import lista_baneados
-from discord.ext import commands,tasks
-from discord import Member
+from discord.ext import commands, tasks
+from webserver import keep_alive
 import TKEN
+import os
 
-status = cycle(["En pruebas","Puedo no funcionar","Cuidado"])
+status = cycle(["Tuki", "tuki", "Melvinator_V.1.2!!"])
 
 class Melvinator_Live(commands.Bot):
     def __init__(self):
         super().__init__(command_prefix=commands.when_mentioned_or('.'), intents=discord.Intents().all())
 
-        self.cogslist = ["cogs.tula","cogs.shipeo","cogs.avatar","cogs.melvin","cogs.ochoball","cogs.MessageEvents","cogs.MelvinGPT"]
-     
-    async def setup_hook(self):
-      for ext in self.cogslist:
-        if ext not in self.extensions:
-          try:
-            await self.load_extension(ext)
-            print(f'Extension {ext} cargada con éxito')
-          except Exception as e:
-             print(f'Error al cargar la extensión {ext}\n{e}')
-        else:
-          print(f'Extension {ext} ya está cargada.')
-
+    async def load_cogs(self):
+        for file in os.listdir("cogs"):
+            if file.endswith(".py"):
+                cog_name = file[:-3]
+                try:
+                    await self.load_extension(f"cogs.{cog_name}")
+                    print(f"Extension {cog_name} loaded successfully")
+                except Exception as e:
+                    print(f"Failed to load extension {cog_name}: {e}")
+    
     @tasks.loop(seconds=5)
     async def change_status(self):
         await self.change_presence(activity=discord.Game(next(status)))
     
     async def on_ready(self):
-        print(f'{self.user} ha entrado!')
-        synced = self.cogslist
-        print("Slash Commands sincronizados:" + str(len(synced)))
+        print(f"{self.user} has logged in.")
+        await self.load_cogs()
         self.change_status.start()
-        await self.setup_hook()
 
-client=Melvinator_Live()
+client = Melvinator_Live()
+
+keep_alive()
 client.run(TKEN.TOKEN)
