cea2c8c790bf32c7581fa00778f4fd2feb36c238
Shiny <106364725+ShinyBread@users.noreply.github.com>
2023-03-20 12:21:24 -0300

    Add files via upload
    MelvinGPT upgraded to gpt-3.5-turbo

--- cogs/MelvinGPT.py
+++ cogs/MelvinGPT.py
@@ -2,29 +2,46 @@ import discord
 import openai
 import openai_API
 from discord.ext import commands
+import os
+import asyncio
+import concurrent
 
 openai.api_key = openai_API.API
 
 class MelvinGPT(commands.Cog):
     def __init__(self, client):
-        self.client= client
+        self.client = client
+        self.conversations = {}
+        self.executor = concurrent.futures.ThreadPoolExecutor(max_workers=1)
+
     @commands.Cog.listener()
-    async def on_message(self,message):
+    async def on_message(self, message):
         if message.author == self.client.user:
             return
-        if message.channel.id==1072614870593323028:
-            if message.content.startswith("!pregunta"):
-                prompt = message.content
+        if message.channel.id == 1072614870593323028:
+            if message.content.endswith("."):
+                channel_id = str(message.channel.id)
+                if channel_id not in self.conversations:
+                    self.conversations[channel_id] = []  # initialize conversation history for this channel
+                mensajes = self.conversations[channel_id]
+                mensaje_de_sistema="Your name is Melvinator and you are friendly and with a lot of sense of humor, your answers are short but precise. Your main language is spanish, but you are very good with other languages."
+                mensajes.append({"role": "system", "content": mensaje_de_sistema })
+                mensaje=message.content
+                mensajes.append({"role": "user", "content": mensaje})
+                
+                # Use run_in_executor to execute the long-running operation in a separate thread
+                response = await asyncio.get_event_loop().run_in_executor(self.executor, self.generate_response, mensajes)
+                
+                respuesta = response["choices"][0]["message"]["content"]
+                mensajes.append({"role": "assistant", "content": respuesta})
+                self.conversations[channel_id] = mensajes  # update conversation history for this channel
+                await message.channel.send(respuesta)
+    
+    def generate_response(self, mensajes):
+        # Call the OpenAI API to generate a response
+        response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=mensajes)
+        return response
 
-                pregunta_melvin = openai.Completion.create(
-                engine="text-davinci-002",
-                prompt=prompt,
-                max_tokens=350,
-                n=1,
-                stop=None,
-                temperature=0.6,
-                ).choices[0].text
-            await message.channel.send(f'MelvinGPT a: {message.author.mention}{pregunta_melvin}')
 
 async def setup(client) -> None:
     await client.add_cog(MelvinGPT(client))
